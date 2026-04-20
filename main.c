08c48a8ef949729531426e533cc443a48c32ae20
dzytko <dawid.zytko@gmail.com>
2026-04-20 22:54:24 +0200

    enable interrupt nesting

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#include "ch32v003fun.h"
 #include "flyback_driver.h"
 #include "i2c.h"
 
 
 int main(void) {
+    uint32_t intsyscr = __get_INTSYSCR();
+    intsyscr |= 1 << 1; // enable interrupt nesting
+    __set_INTSYSCR(intsyscr);
+
     int ret = i2c_init();
     while (ret < 0) {
         (void)0;
